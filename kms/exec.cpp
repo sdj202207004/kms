bc9df80dad2b1f31baae19585ddf0685474545e5
sdj202207004 <shimdj0425@gmail.com>
2025-06-14 21:56:19 +0900

    1. kms/exec.cpp [ + ] Add exec

--- kms/exec.cpp
+++ kms/exec.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include "pri.h"
+#include "api.h"
 //강민수 브랜치명 : dev/api
 //파일 : api.cpp, api.h
 
@@ -9,9 +10,13 @@
 
 int main() {
 
+	api api;
+	api.initUser();
+	api.randomCom();
+	pri pri(api);
 
-
-
-
+	pri.printUser();
+	pri.printCom();
+	pri.printResult();
 	return 0;
 }
