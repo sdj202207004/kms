60115975239a7b41f47f2eb3340695d040450c0f
sdj202207004 <shimdj0425@gmail.com>
2025-06-14 21:51:34 +0900

    1. kms/pri.h [ + ] class pri header file 2. kms/pri.cpp [ + ] define class pri

--- kms/pri.h
+++ kms/pri.h
@@ -1,5 +1,18 @@
 #pragma once
+#include "api.h"
+
 class pri
 {
+private:
+	int user;
+	int com;
+	int result;
+public:
+	pri(api);
+	~pri();
+
+	void printUser();
+	void printCom();
+	void printResult();
 };
 
